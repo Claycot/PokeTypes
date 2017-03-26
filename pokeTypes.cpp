3d6dfd7a90f6b3a723d05f0bfa7c2fcd108df20b
Clay <Claycot@users.noreply.github.com>
2017-03-26 08:46:45 -0400

    Update pokeTypes.cpp
    Add CLI version and comments

--- pokeTypes.cpp
+++ pokeTypes.cpp
@@ -1,20 +1,29 @@
-// pokemonTypes.cpp : Defines the entry point for the console application.
-// Code by Claycot
+//Code by claycot
 
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <sstream>
 
 using namespace std;
-int matchType(string inputType, string libTypes[19]);
+
+//Prototype functions
+int matchType(string inputType, string libTypes[18]);
 string argvToStr(char *argv[], unsigned int argNum);
+string strToLwr(string inputString);
 float damageMult(int atkDefTypes[], int effec[18][18]);
+void showHelp();
+void showTypes(string types[18]);
 
 int main(int argc, char *argv[])
 {
-	string types[19] = {"normal", "fire", "water", "electric", "grass", "ice", 
+	//Define all of the pokemon types
+	string types[18] = {"normal", "fire", "water", "electric", "grass", "ice", 
 		"fighting", "poison", "ground", "flying", "psychic", "bug", "rock", 
 		"ghost", "dragon", "dark", "steel", "fairy"};
+	
+	//Grid of effectiveness. Actual effectiveness is half of these values
+	//Order for columns is same as order for rows
 	int effectivity[18][18] = {
 		/*norm*/ {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 2, 2, 1, 2},
 		/*fire*/ {2, 1, 1, 2, 4, 4, 2, 2, 2, 2, 2, 4, 1, 2, 1, 2, 4, 2},
@@ -35,35 +44,103 @@ int main(int argc, char *argv[])
 		/*stel*/ {2, 1, 1, 1, 2, 4, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 1, 4},
 		/*fair*/ {2, 1, 2, 2, 2, 2, 4, 1, 2, 2, 2, 2, 2, 2, 4, 4, 1, 2}
 	};
+
+	//Initialize the type matches as -1 (error/not found)
 	int inputTypes[3] = { -1, -1 , -1 };
 
+	//Check arguments, use this if not wanting direct input
 	if (argc < 3 || argc > 4) {
+
+		//If started without arguments, enter into CLI version
 		if (argc == 1) {
-			cout << "Please run this program from the command line.\n"
-				<< "Run the program with the following command for help.\n"
-				<< "\"help\"";
-			cin.ignore();
+
+			//Welcome message and show help
+			cout << "Welcome to the Pokemon effectiveness calculator!\n";
+			showHelp();
+			cout << "Enter the letter Q at any time to quit.\n";
+
+			//Initialize user inputs
+			string userInput = "";
+			string buffer[3] = { "", "", "" };
+
+			//Get user input until user wants to quit
+			do {
+				
+				//Prompt user and receive input
+				userInput = "";
+				cout << "\nEnter the Pokemon types: ";
+				getline(cin, userInput);
+
+				//If user requests types, display them
+				if (userInput.substr(0, 5) == "types" || userInput.substr(0, 5) == "TYPES") {
+					showTypes(types);
+				}
+
+				//Process user input
+				else {
+					stringstream input(userInput);
+
+					//For the first three arguments in the string stream,
+					//Process and match the types
+					for (int i = 0; i < 3; i++) {
+						buffer[i] = "";
+						input >> buffer[i];
+
+						//Convert the input type to lower for matching
+						buffer[i] = strToLwr(buffer[i]);
+
+						if (buffer[i] != "") {
+							inputTypes[i] = matchType(buffer[i], types);
+						}
+						else {
+							inputTypes[i] = -1;
+						}
+					}
+
+					//Output effectiveness if not an error
+					if (inputTypes[0] != -1 && inputTypes[1] != -1) {
+						cout << "Attacker: " << types[inputTypes[0]] << "\n"
+							<< "Defender: " << types[inputTypes[1]];
+
+						//If a third type was entered, output that too
+						if (inputTypes[2] != -1) {
+							cout << " & " << types[inputTypes[2]];
+						}
+						cout << "\n"
+							<< "Multiplier: "
+							<< damageMult(inputTypes, effectivity)
+							<< "x\n";
+					}
+				}
+			} while (buffer[0] != "q" && buffer[0] != "Q");
 		}
+
+		//Process user help request
 		else if (argvToStr(argv, 1) == "help") {
-			cout << "Please follow one of the formats below...\n"
-				<< "<attacking type> <defending type>\n"
-				<< "<attacking type> <defending type 1> <defending type 2>\n"
-				<< "(Run with command \"types\" for a list of types)\n";
+			showHelp();
 		}
+
+		//Process user types request
 		else if (argvToStr(argv, 1) == "types") {
-			for (int i = 0; i < 17; i++) {
-				cout << types[i] << ", ";
-			}
-			cout << types[17] << endl;
+			showTypes(types);
 		}
+
+		//Catch-all for bad inputs
 		else {
 			cout << "Usage error. Run with command \"help\" for guidance." << endl;
 		}
 	}
 
+	//Process direct input
 	if (argc >= 3 || argc <= 4) {
+		
+		//Switch for number of arguments
 		switch (argc) {
+		
+		//If 3 arguments, process the 1-attack, 1-defense calculation
 		case 3:
+			
+			//Match each argument to a type
 			for (int argNum = 1; argNum <= 2; argNum++) {
 				inputTypes[argNum - 1] = matchType(argvToStr(argv, argNum), types);
 				if (inputTypes[argNum - 1] == -1) {
@@ -71,19 +148,27 @@ int main(int argc, char *argv[])
 				}
 			}
 			inputTypes[2] = -1;
+
+			//Output the result
 			cout << "Attacker: " << types[inputTypes[0]] << "\n"
 				<< "Defender: " << types[inputTypes[1]] << "\n"
 				<< "Multiplier: "
 				<< damageMult(inputTypes, effectivity)
 				<< "x\n";
 				break;
+
+		//If 4 arguments, process the 1-attack, 2-defense calculation
 		case 4:
+
+			//Match each argument to a type
 			for (int argNum = 1; argNum <= 3; argNum++) {
 				inputTypes[argNum - 1] = matchType(argvToStr(argv, argNum), types);
 				if (inputTypes[argNum - 1] == -1) {
 					return -1;
 				}
 			}
+
+			//Output the result
 			cout << "Attacker: " << types[inputTypes[0]] << "\n"
 				<< "Defender: " << types[inputTypes[1]] << " & " 
 				<< types[inputTypes[2]] << "\n"
@@ -96,21 +181,28 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-int matchType(string inputType, string libTypes[19]) {
-	for (int ind = 0; ind < 19; ind++) {
+
+//Match the user input to a valid pokemon type
+int matchType(string inputType, string libTypes[18]) {
+	
+	//Loop through all types searching for a match
+	for (int ind = 0; ind < 18; ind++) {
 		if (inputType == libTypes[ind]) {
 			return ind;
 		}
 	}
 
+	//Output error message if not found
 	cout << "Error: type " << inputType << " not found.\n"
 		<< "Run the program with argument \"types\" to view.\n";
 	return -1;
 }
 
+//Convert an argument to a lowercase string
 string argvToStr(char *argv[], unsigned int argNum) {
 	string outputStr = argv[argNum];
 
+	//For each character in the argument, add to string
 	for (unsigned int let = 0; let <= outputStr.length(); let++) {
 		outputStr[let] = tolower(outputStr[let]);
 	}
@@ -118,19 +210,55 @@ string argvToStr(char *argv[], unsigned int argNum) {
 	return outputStr;
 }
 
+//Convert a string to a lowercase string
+string strToLwr(string inputString) {
+	string outputStr = inputString;
+
+	//For each character in the argument, add to string
+	for (unsigned int let = 0; let <= inputString.length(); let++) {
+		inputString[let] = tolower(inputString[let]);
+	}
+
+	return inputString;
+}
+
+//Calculate the damage multiplier
 float damageMult(int atkDefTypes[], int effec[18][18]) {
+	
+	//Initialize the two multipliers
 	float mult1 = 2;
 	float mult2 = 2;
 	cout << setprecision(2);
 
+	//Calculate the first multiplier
 	mult1 = effec[atkDefTypes[0]][atkDefTypes[1]];
-	//cout << "Mult 1 = " << mult1 << endl;
-	if (atkDefTypes[2] == -1) {
+
+	//If no second defending type, return the primary result
+	if (atkDefTypes[2] == -1 || atkDefTypes[1] == atkDefTypes[2]) {
 		return mult1 / 2;
 	}
+
+	//If a second defending type, calculate for that scenario
 	else {
 		mult2 = effec[atkDefTypes[0]][atkDefTypes[2]];
-		//cout << "Mult 2 = " << mult2 << endl;
+
+		//Return the result
 		return mult1 * mult2 / 4;
 	}
+}
+
+//Draw the help menu
+void showHelp() {
+	cout << "Please follow one of the formats below...\n"
+		<< "<attacking type> <defending type>\n"
+		<< "<attacking type> <defending type 1> <defending type 2>\n"
+		<< "(Run with command \"types\" for a list of types)\n";
+}
+
+//Draw each of the pokemon types
+void showTypes(string types[18]) {
+	for (int i = 0; i < 17; i++) {
+		cout << types[i] << ", ";
+	}
+	cout << types[17] << endl;
 }
